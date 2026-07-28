5a8d69bf718f83a81fc5502d1ed71dff21b49991
Paul7077 <npaulprasannakumar@gmail.com>
2026-07-29 00:13:22 +0530

    update tem,plate

---
+++ core/app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from app import views
+urlpatterns = [
+    path('', views.Home, name="Home"),
+    path('about/', views.About, name="about"),
+    path('login/', views.Login, name="login"),
+    path('register/', views.Register, name="login"),
+]
--- core/app/views.py
+++ core/app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Home(request): 
+    return render(request, "uifiles/index.html")
+
+
+def About(request): 
+    return render(request, "uifiles/about.html")
+
+
+def Login(request): 
+    return render(request, "uifiles/login.html")
+
+def Register(request): 
+    return render(request, "uifiles/register.html")
+
+def Login(request): 
+    return render(request, "uifiles/login.html")
